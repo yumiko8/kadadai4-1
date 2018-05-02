3254777b3ca08769f06ad83243c7df51439a6e8a
g16953yy <g16953yy@s20540.tsuda.ac.jp>
2018-05-02 11:51:58 +0900

    snakecamelutil

---
+++ test/junit/tutorial/SnakeCamelUnitTest.java
@@ -0,0 +1,14 @@
+package junit.tutorial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnakeCamelUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
